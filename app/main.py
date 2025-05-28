4db19aec8c4bc38e24a53ee798d42083fba9d048
ITPartner75 <itpartner41@gmail.com>
2025-05-28 11:39:51 +0530

    Added allowed origins

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.session import engine
 from app.db.base import Base  # This includes your models
 from app.api.v1.auth import router as auth_router
@@ -6,6 +7,18 @@ from app.api.v1.business import router as business_router
 
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ✅ Create tables on startup
 @app.on_event("startup")
